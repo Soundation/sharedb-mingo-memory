739a5078cf63c630e7cb52f042757a94558d1168
Greg Kubisa <gkubisa@gmail.com>
2018-07-20 15:15:58 +0200

    Update dependencies

--- test/test.js
+++ test/test.js
@@ -17,7 +17,16 @@ describe('db', function() {
   describe('query', function() {
     require('./query')();
 
-    it('unsupported', function() {
+    // No idea how query was supposed to fail with an "Unsupported" message.
+    // I searched the whole history of mingo and the word "Unsupported" does appear there at all.
+    // I searched using:
+    // git log -GUnsupported --all
+    // git log -SUnsupported --all
+    // Before updating mocha to 5.2.0 this test was a false positive
+    // because mocha didn't crash on the uncaught assertion error.
+    // Notice there is no `done` callback, so the test passed synchronously
+    // and the assertion error was thrown asynchronously.
+    it.skip('unsupported', function() {
       this.db.query('testcollection', {$mapReduce: []}, null, null, function(err, results) {
         expect(err).ok();
         expect(err.message).match(/Unsupported/);
